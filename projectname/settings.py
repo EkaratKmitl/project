4dbe5cc07953f037fd91e4d15d5632bbfb56f3cc
EkaratKmitl <ekarat.income@gmail.com>
2024-05-09 13:17:29 +0700

    Traning Github

--- projectname/settings.py
+++ projectname/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "blog",  # ประกาศว่ามีแอปชื่อนี้ เพิ่มขึ้นมา
+    "store",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
